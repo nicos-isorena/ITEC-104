a798de0c17721a2946decb846047da5adbd53430
nicos-isorena <112452492+nicos-isorena@users.noreply.github.com>
2022-09-19 20:28:07 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+namespace ITEC104_ACTIVITY3_ISORENA 
+{ 
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] emails = { "nicosisorena22@gmail.com",
+                    "its_hades@gmail.com",
+                    "Callme_Zeus.@gmailcom",
+                                             };
+
+
+            string[] username = {"Its_Nicozzz",
+                     "Hades_19",
+                     "LightningZeus",
+                                           };
+
+
+            string[] passwords = { "12345",
+                       "password888",
+                       "guesswhat",
+                                     };
+
+            Console.Write(" Pick a number from (0-2) for Index : ");
+
+            int index = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("EMAIL    : " + emails[index]);
+            Console.WriteLine("USERNAME : " + username[index]);
+            Console.WriteLine("PASSWORD : " + passwords[index]);
+        }
+    }
+}
