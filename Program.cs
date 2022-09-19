128ed9e16ee9c56e885f8c9effe8781d38692460
nicos-isorena <112452492+nicos-isorena@users.noreply.github.com>
2022-09-19 20:24:40 +0800

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,38 +1,53 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-using System;
 
-namespace ITEC104_ACTIVITY3_ISORENA 
-{ 
+ int[] num = new int[5];
 
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            string[] emails = { "nicosisorena22@gmail.com",
-                    "its_hades@gmail.com",
-                    "Callme_Zeus.@gmailcom",
-                                             };
+int i;
 
+Console.WriteLine("==ARRAY PROGRAM==");
 
-            string[] username = {"Its_Nicozzz",
-                     "Hades_19",
-                     "LightningZeus",
-                                           };
-
-
-            string[] passwords = { "12345",
-                       "password888",
-                       "guesswhat",
-                                     };
-
-            Console.Write(" Pick a number from (0-2) for Index : ");
-
-            int index = Convert.ToInt32(Console.ReadLine());
-
-            Console.WriteLine("EMAIL    : " + emails[index]);
-            Console.WriteLine("USERNAME : " + username[index]);
-            Console.WriteLine("PASSWORD : " + passwords[index]);
-        }
-    }
+for (i = 0; i < 5; i++)
+{
+    Console.Write("ENTER ELEMENT [{0}] : ", i);
+    num[i] = Convert.ToInt32(Console.ReadLine());
 }
+
+Console.Write("\nOdd Numbers: ");
+
+for (i = 0; i < 5; i++)
+{
+    if (num[i] % 2 == 1)
+        Console.Write("{0} ", num[i]);
+}
+
+Console.Write("\nEven Numbers: ");
+
+for (i = 0; i < 5; i++)
+{
+    if (num[i] % 2 == 0)
+        Console.Write("{0} ", num[i]);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
